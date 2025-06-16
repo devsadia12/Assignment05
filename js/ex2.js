ea8afb2428f7044a12f76b6555df18a893490b4f
devsadia12 <Afrosesadia75@gmail.com>
2025-06-16 20:17:00 +0600

    2nd solu done

---
+++ js/ex2.js
@@ -0,0 +1,8 @@
+
+
+
+
+
+ var classification = [ "John Hill" , "Mary Jane", "Gary Vee", "Paricia Mills", "Helen Hall", "Paul Green" ];
+ var bestThree = classification.slice(-3);
+  document.getElementById("best_students").innerHTML = bestThree;
---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+document.getElementById("blue_box").innerHTML;
+//console.log( document.getElementById("blue_box").innerHTML);
+document.getElementById("blue_box").innerHTML = "Hi there!";
+//console.log(document.getElementById("blue_box").innerHTML);
+
+var user_name = "guest user";
+//console.log(user_name);
+document.getElementById("user_greeting_message").innerHTML = "Hello" + user_name + "!";
+
+document.getElementById
